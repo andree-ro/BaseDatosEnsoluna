ee468c41d988b22218a0f3dc328934453a977c75
pacoti <pca2003gte@gmail.com>
2023-04-27 21:50:42 -0600

    TRIGGER UPDATE

--- sql_structures/dbs/TRIGGERS.sql
+++ sql_structures/dbs/TRIGGERS.sql
@@ -5,8 +5,9 @@ DROP TRIGGER IF EXISTS reducir_existencias_inventario//
 
 CREATE TRIGGER reducir_existencias_inventario BEFORE INSERT ON Venta_has_Cafe FOR EACH ROW
 BEGIN
-	UPDATE Cafe SET Libras = Libras - NEW.cantidad
-    WHERE id = NEW.Cafe_idCafe;
+	UPDATE Cafe SET Libras = Libras - NEW.cantidad WHERE id = NEW.Cafe_idCafe;
+    
+    UPDATE Empacado SET Cantidad = Cantidad - NEW.cantidad WHERE id = NEW.Empacado_idEmpacado;
 
 END//
 
